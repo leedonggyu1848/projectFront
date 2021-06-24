46586ef8531b7c316d9154943df12ddac2210a55
donggyu <leedong1848@gmail.com>
2021-06-24 10:47:05 +0900

    apexchart package에 추가

--- src/UserResult.jsx
+++ src/UserResult.jsx
@@ -20,7 +20,6 @@ const useStyles = makeStyles((theme) => ({
       position: 'unset',
       width: '100vw',
       height: 'unset',
-      position: 'unset',
       left: 'unset',
       overflow:'unset'
     }
