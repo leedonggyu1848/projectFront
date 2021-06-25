b069d928c3ea6e9f230a4e8dafeba9c8a3e27bc8
donggyu <leedong1848@gmail.com>
2021-06-25 12:48:27 +0900

    atom default관련 주석 추가

--- src/atom.jsx
+++ src/atom.jsx
@@ -1,18 +1,21 @@
 import { atom } from "recoil";
 
 // 키 정보
+//default: number;
 const userHeight = atom({
   key: 'userHeight',
   default: 0
 })
 
 // 몸무게 정보
+//default: number;
 const userWeight = atom({
   key:'userWeight',
   default: 0
 })
 
 // 부대 정보
+//default: string; (육군훈련소가 들어가서 string으로 바꿈)
 const userSection = atom({
   key: 'userSection',
   default: '8623'
