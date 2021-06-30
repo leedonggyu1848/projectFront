59bd35e5035c311a79215bb795ead2396d9a4ab1
murry2018 <2clean8@naver.com>
2021-06-30 23:49:20 +0900

    Menu feature

--- src/MenuInfo.jsx
+++ src/MenuInfo.jsx
@@ -21,41 +21,103 @@ const sectionCode={
   '7652': null
 };
 
-//schema base url
+// schema base url
 const schemaUrl = 'https://infuser.odcloud.kr/oas/docs'
 
-//api base url
+// api base url
 const apiUrl = 'https://api.odcloud.kr/api'
 
-//section에 맞는 url을 넘겨줍니다.
+// section에 맞는 url을 넘겨줍니다.
 function getSchemaUrl(section){
   const url = schemaUrl + '?namespace='+ sectionCode[section]+'/v1';
   return url + '&serviceKey=' + process.env.REACT_APP_ENCODING;
 }
 
-//section의 메뉴 데이터를 넘겨줍니다
-//비동기함수 입니다.
+// section의 메뉴 데이터를 넘겨줍니다
 async function getMenuData(section){
   let schema;
   let dataRes;
 
-  try{
+  try {
     schema = await axios.get(getSchemaUrl(section))
-  }catch(e){
+  } catch (e) {
     console.log(e, 'schema error');
-    throw(e);
+    throw (e);
   }
+  
   const dataPhath = Object.keys(schema.data.paths)[0];
-  try{
+  try {
     dataRes = await axios.get(apiUrl + dataPhath + 
       '?serviceKey=' + process.env.REACT_APP_ENCODING + '&perPage=1000')
-  }catch(e){
+  } catch(e) {
     console.log(e, 'api error');
-    throw(e);
+    throw (e);
   }
   return dataRes.data;
 }
 
+// 짬
+class Trash {
+  constructor(menu, calorie) {
+    this.menu = menu;
+    this.calorie = calorie
+  }
+}
+
+// 짬통
+class TrashBag {
+  constructor() {
+    this.breakfastMenus = [];
+    this.lunchMenus     = [];
+    this.dinnerMenus    = [];
+    this.overallCalorie = 0;
+  }
+  addBreakfast(aTrash) {
+    if (aTrash.menu) {
+      this.breakfastMenus.push(aTrash);
+    }
+    return this;
+  }
+  addLunch(aTrash) {
+    if (aTrash.menu) {
+      this.lunchMenus.push(aTrash);
+    }
+    return this;
+  }
+  addDinner(aTrash) {
+    if (aTrash.menu) {
+      this.dinnerMenus.push(aTrash);
+    }
+    return this;
+  }
+  addCalorie(calorie) {
+    if (calorie)
+      this.overallCalorie += calorie;
+    return this;
+  }
+};
+
+function trashBagFromMilitaryFormat(data) {
+  const trashBag = new TrashBag();
+  for (const item of data) {
+    trashBag
+      .addBreakfast(
+        new Trash(item['조식'], parseFloat(item['조식열량'])))
+      .addLunch(
+        new Trash(item['중식'], parseFloat(item['중식열량'])))
+      .addDinner(
+        new Trash(item['석식'], parseFloat(item['석식열량'])))
+      .addCalorie(parseFloat(item['열량합계']) || 0);
+  }
+  return trashBag;
+}
+
+/*
+  props: {
+    date: string ("yyyy-mm-dd");
+    section: string (see UserForm.jsx$availableSection)
+  }
+ */
 export default function MenuInfo(props){
   //api 요청이 끝났는지 확인을 위한 변수
   const [apiState, setApiState] = useState(0);
@@ -63,38 +125,92 @@ export default function MenuInfo(props){
   const section = useRecoilValue(userSection);
   //받은 데이터
   const [data, setData] = useState('null');
-
+  let meal =  // 이건 그냥 예시입니다.
+    new TrashBag()
+      .addBreakfast(new Trash('전투식량1형', 1100))
+      .addLunch(new Trash('전투식량2형', 1100))
+      .addDinner(new Trash('군용쌀건빵', 450))
+      .addCalorie(2650);
   //section이 바뀌면 실행됩니다.
+
   //console에 data를 출력하는 부분은 완성되면 지워야 합니다.
-  useEffect(()=>{
+  useEffect(() => {
     setApiState(0);
-    (async ()=>{
-      try{
-        let data = await getMenuData(section);
-        setData(data.data[0]['석식']);
+    const dropData = async () => {
+      try {
+        const data = await getMenuData(section);
+        setData(data.data);
+        
         console.log(data);
         setApiState(1);
-      }catch(e){
+      } catch (e) {
         console.log(e)
         setApiState(2);
       }
-    })();
-  }, [section, setApiState, setData])
-
-  if (apiState === 0){
-    return(
+    };
+    dropData();
+  }, [section])
+  
+  if (apiState === 0) {
+    return (
       <div>
-      <span>loading...</span>
+        <span>loading...</span>
       </div>
     )
-  }else if(apiState === 1){
-    return(
+  } else if (apiState === 1) {
+    // It's a puzzle! Guess what this statement means ;P
+    const day = ((splittedDate) =>
+      (
+        (parseInt(splittedDate[splittedDate.length - 1]) - 1)
+          % 28 + 1
+      ).toString().padStart(2,'0')
+    )(props.date.split('-'));
+    
+    const todayData = data.filter((item) => item['날짜'].endsWith(day));
+    meal = trashBagFromMilitaryFormat(todayData);
+    
+    const breakfastElem = meal.breakfastMenus.map(
+      (aTrash) => <li key={'조' + aTrash.menu}>{aTrash.menu}</li>
+    );
+    const lunchElem = meal.lunchMenus.map(
+      (aTrash) => <li key={'중' + aTrash.menu}>{aTrash.menu}</li>
+    );
+    const dinnerElem = meal.dinnerMenus.map(
+      (aTrash) => <li key={'동' + aTrash.menu}>{aTrash.menu}</li>
+    );
+    return (
       <div>
-        <span>{data}</span> 
+        <div>
+          <div>
+            조식
+          </div>
+          <ul style={{listStyleType: 'none'}}>
+            {breakfastElem}
+          </ul>
+        </div>
+        <div>
+          <div>
+            중식
+          </div>
+          <ul style={{listStyleType: 'none'}}>
+            {lunchElem}
+          </ul>
+        </div>
+        <div>
+          <div>
+            석식
+          </div>
+          <ul style={{listStyleType: 'none'}}>
+            {dinnerElem}
+          </ul>
+        </div>
+        <div>
+          총 칼로리는 {meal.overallCalorie}kcal 입니다.
+        </div>
       </div>
     )
-  }else if(apiState === 2){
-    return(
+  } else if (apiState === 2) {
+    return (
       <div>
         <span>error!</span>
       </div>
